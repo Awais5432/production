1c4a76d31f56bb684dde047b67354b6dbe95f200
Awais Gohar <awaisgohar5432@gmail.com>
2020-01-05 23:05:00 +0500

    Updates

---
+++ precache-manifest.66b91a7f501c8a9f527cc620000969d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a7ffa7161ecdee0fe68bcbc619e7e2",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "5d102597e86542a40fd5",
+    "url": "/production/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "42cbb43a791c262a204b",
+    "url": "/production/static/css/main.9fd4efb9.chunk.css"
+  },
+  {
+    "revision": "5d102597e86542a40fd5",
+    "url": "/production/static/js/2.34669da3.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/production/static/js/2.34669da3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42cbb43a791c262a204b",
+    "url": "/production/static/js/main.a8c310af.chunk.js"
+  },
+  {
+    "revision": "0d7774ee4eb9e89f40c3",
+    "url": "/production/static/js/runtime-main.fd31cc2e.js"
+  }
+]);
